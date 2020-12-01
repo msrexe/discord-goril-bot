88ba3de7ce160bc37f62f23ff90fe339a01c6f86
Melih Sivri <mlheymen.ms@gmail.com>
2020-12-02 02:53:46 +0300

    Update db.js

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const readFile = () => {
   return new Promise((resolve) => {
     fs.readFile('memes.txt','utf-8', (err,data) => {
       if(err) {
+        console.log('db açamadım');
         throw err;
       }
       resolve(data);
